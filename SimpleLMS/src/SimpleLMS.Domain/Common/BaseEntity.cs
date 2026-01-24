d6e4e940700bcc298d9e9cc941cbfb34c87123d8
Nu <cuthanhcam04@gmail.com>
2026-01-24 13:48:00 +0700

    feat: add Domain/Common, Entities, Enums - implement basic of User, Course, Lesson, Enrollment - add Property and Navigation properties

---
+++ SimpleLMS/src/SimpleLMS.Domain/Common/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleLMS.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set } = Guid.NewGuid();
+        public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; protected set; }
+        public bool IsDeleted { get; protected set; }
+
+        public virtual void Delete()
+        {
+            IsDeleted = true;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        protected void MarkAsModified()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
