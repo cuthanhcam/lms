551393be4fc68df7d95869ed1ba0550669f4d595
Nu <cuthanhcam04@gmail.com>
2026-01-25 20:33:53 +0700

    feat: add Application/Interfaces/Repositories - implement Unit Of Work Pattern - Ensure all repository operations are performed within a single   transaction

---
+++ SimpleLMS/src/SimpleLMS.Application/Interfaces/Repositories/ICourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using SimpleLMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleLMS.Application.Interfaces.Repositories
+{
+    public interface ICourseRepository : IRepository<Course>
+    {
+        Task<IEnumerable<Course>> GetPublishedCoursesAsync();
+        Task<IEnumerable<Course>> GetCoursesByInstructorAsync(Guid instructorId);
+        Task<Course?> GetCourseWithLessonsAsync(Guid courseId);
+        Task<Course?> GetCourseWithEnrollmentsAsync(Guid courseId);
+    }
+}
---
+++ SimpleLMS/src/SimpleLMS.Application/Interfaces/Repositories/IEnrollmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using SimpleLMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleLMS.Application.Interfaces.Repositories
+{
+    public interface IEnrollmentRepository : IRepository<Enrollment>
+    {
+        Task<IEnumerable<Enrollment>> GetEnrollmentsByUserAsync(Guid userId);
+        Task<IEnumerable<Enrollment>> GetEnrollmentsByCourseAsync(Guid courseId);
+        Task<Enrollment?> GetEnrollmentWithDetailsAsync(Guid enrollmentId);
+        Task<bool> UserAlreadyEnrolledAsync(Guid userId, Guid courseId);
+    }
+}
