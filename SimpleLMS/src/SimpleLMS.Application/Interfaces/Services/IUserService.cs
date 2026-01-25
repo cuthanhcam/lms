fd9484483940b6b4e342c926041af30b81094383
Nu <cuthanhcam04@gmail.com>
2026-01-25 23:52:18 +0700

    feat: add Application/Interfaces/Services

---
+++ SimpleLMS/src/SimpleLMS.Application/Interfaces/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using SimpleLMS.Application.Common;
+using SimpleLMS.Application.DTOs.Users;
+
+namespace SimpleLMS.Application.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<Result<UserDto>> GetByIdAsync(Guid id);
+        Task<Result<IEnumerable<UserDto>>> GetAllAsync();
+        Task<Result<UserDto>> CreateAsync(CreateUserDto dto);
+        Task<Result<UserDto>> UpdateAsync(Guid id, UpdateUserDto dto);
+        Task<Result> DeleteAsync(Guid id);
+        Task<Result<LoginResponseDto>> LoginAsync(LoginDto dto);
+    }
+}
