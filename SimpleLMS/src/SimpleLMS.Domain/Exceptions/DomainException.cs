b0592af2b3d98fddd6bb63643b67da6f1ab96436
Nu <cuthanhcam04@gmail.com>
2026-01-25 15:58:54 +0700

    feat: Add Domain/Exceptions and update Entities for rich domain models

---
+++ SimpleLMS/src/SimpleLMS.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleLMS.Domain.Exceptions
+{
+    /// <summary>
+    /// Base exception for domain-related errors.
+    /// </summary>
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
