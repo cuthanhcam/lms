8bb168715c2230d2636023828dce685349ab4aab
Nu <cuthanhcam04@gmail.com>
2026-01-27 14:43:47 +0700

    feat: implement Application/../ILessonSerivce, IEnrollmentService

---
+++ SimpleLMS/src/SimpleLMS.Application/Interfaces/Services/IEnrollmentService.cs
@@ -0,0 +1,16 @@
+﻿using SimpleLMS.Application.Common;
+using SimpleLMS.Application.DTOs.Enrollments;
+
+namespace SimpleLMS.Application.Interfaces.Services
+{
+    public interface IEnrollmentService
+    {
+        Task<Result<EnrollmentDto>> GetByIdAsync(Guid id);
+        Task<Result<IEnumerable<EnrollmentDto>>> GetEnrollmentsByUserAsync(Guid userId);
+        Task<Result<IEnumerable<EnrollmentDto>>> GetEnrollmentsByCourseAsync(Guid courseId);
+        Task<Result<EnrollmentDto>> EnrollAsync(Guid userId, CreateEnrollmentDto dto);
+        Task<Result> UpdateProgressAsync(Guid enrollmentId, Guid userId, UpdateProgressDto dto);
+        Task<Result> CompleteAsync(Guid enrollmentId, Guid userId);
+        Task<Result> CancelAsync(Guid enrollmentId, Guid userId);
+    }
+}
